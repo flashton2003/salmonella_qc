90782df4a15ca9cccdac4cb915b49e39de1cfcab
flashton2003 <flashton@gmail.com>
2020-10-02 14:22:57 +0200

    going to change how making master df

--- interpret_kraken2_output.py
+++ interpret_kraken2_output.py
@@ -2,7 +2,6 @@ import string
 import pandas as pd
 
 class KrakenResult():
-
     def __init__(self, split_line):
         self.sample_name = split_line[0]
         self.percent_reads_assigned = float(split_line[1])
@@ -14,21 +13,46 @@ class KrakenResult():
         self.name = split_line[6].strip()
 
 def read_in_parsed_kraken_result(kraken_inhandle):
-    # output_dict = {}
-    # with open(kraken_inhandle) as fi:
-        # for line in fi.readlines():
-            # print(line.strip())
-            # split_line = line.strip().split('\t')
-            # kraken_result = KrakenResult(split_line)
     kraken_results = pd.read_csv(kraken_inhandle, sep = '\t', header = None)
-    
+    return kraken_results
 
+def make_output_dataframe(kraken_results):
+    kraken_interpretation = pd.DataFrame()
+    kraken_interpretation['sample_name'] = kraken_results[0].unique().tolist()
+    return kraken_interpretation
+
+def get_count_column(contam):
+    output_list = []
+    counting_dict = {}
+    for name in contam[0]:
+        if name in counting_dict:
+            counting_dict[name] += 1
+            output_list.append(counting_dict[name])
+        else:
+            counting_dict[name] = 1
+            output_list.append(counting_dict[name])
+    return output_list
+
+def identify_contaminated_samples(kraken_results):
+    contam = kraken_results.loc[kraken_results[6] != 'Salmonella enterica']
+    contam = contam.assign(contam_string = [f'{x}; {y}' for x, y in zip(contam[6], contam[1])])
+    contam = contam[[0, 'contam_string']]
+    ## need to add a column index, in addition to the "row" index which will be the sample name
+    count_column = get_count_column(contam)
+    contam = contam.assign(count_column = count_column)
+    contam = contam.pivot(index = 0, columns = 'count_column', values = 'contam_string')
+    return contam
 
 def main(kraken_inhandle):
-    read_in_parsed_kraken_result(kraken_inhandle)
+    kraken_results = read_in_parsed_kraken_result(kraken_inhandle)
+    kraken_interpretation = make_output_dataframe(kraken_results)
+    contam = identify_contaminated_samples(kraken_results)
+    # with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        # print(contam)
 
 root_dir = '/Users/flashton/Dropbox/GordonGroup/ben_kumwenda_genomes'
-kraken_inhandle = f'{root_dir}/kraken2/results/2020.09.30/2020.10.01.parsed_results.txt'
+kraken_inhandle = f'{root_dir}/kraken2/results/2020.09.30/2020.10.01.parsed_results.v2.txt'
 
 if __name__ == '__main__':
     main(kraken_inhandle)
+
--- salmonella_qc.py
+++ salmonella_qc.py
@@ -1,9 +1,41 @@
+import pandas as pd
+import interpret_kraken2_output
 import docopt
 
-class Sample():
+def run_interpret_kraken2_output(kraken_inhandle):
+    kraken_results = interpret_kraken2_output.read_in_parsed_kraken_result(kraken_inhandle)
+    kraken_interpretation = interpret_kraken2_output.make_output_dataframe(kraken_results)
+    contam = interpret_kraken2_output.identify_contaminated_samples(kraken_results)
+    # print(contam.index)
+    return contam
 
+def read_sistr(sistr_inhandle):
+    # with open(sistr_inhandle) as fi:
+    sistr_results = pd.read_csv(sistr_inhandle, sep = '\t')
+    sistr_results = sistr_results[['genome', 'qc_messages', 'qc_status', 'serovar', 'serovar_antigen', 'serovar_cgmlst']]
+    # print(sistr_results[['genome']])
+    return sistr_results
 
-def main():
+def read_assembly_stats(assembly_stats_inhandle):
+    assembly_stats_results = pd.read_csv(assembly_stats_inhandle, sep = '\t')
+    ## since just catted together all the outputs, there is a header for each one.
+    assembly_stats_results = assembly_stats_results.loc[assembly_stats_results['filename'] != 'filename']
+    assembly_stats_results = assembly_stats_results.assign(sample_name = [x.split('/')[-1].rstrip('_contigs.fa') for x in assembly_stats_results['filename']])
+    assembly_stats_results = assembly_stats_results[['sample_name', 'total_length', 'number', 'N50']]
+    return assembly_stats_results
+
+def make_master_df(contam_samples, sistr_samples, assembly_stats_samples):
+    all_sample_names = list(set(contam_samples).union(set(sistr_samples), set(assembly_stats_samples)))
+    # print(len(all_samples))
+    # print(all_samples)
+    ## not sure how genome snuck in there, from sistr results probably
+    if 'genome' in all_sample_names:
+        all_sample_names.remove('genome')
+    all_samples = pd.DataFrame({'samples':all_sample_names})
+    # print(all_samples)
+    return all_samples
+
+def main(kraken_inhandle, sistr_inhandle, assembly_stats_inhandle):
     '''
     1. read in
         a. sistr results
@@ -13,12 +45,19 @@ def main():
     3. combine a, b, c
     4. make decision about each one
     '''
-    pass
+    contam = run_interpret_kraken2_output(kraken_inhandle)
+    sistr_results = read_sistr(sistr_inhandle)
+    assembly_stats_results = read_assembly_stats(assembly_stats_inhandle)
+    # print(contam[[0]])
+    all_samples = make_master_df(list(contam.index), sistr_results[['genome']], assembly_stats_results['sample_name'])
+    # print(contam)
+    
+
 
 root_dir = '/Users/flashton/Dropbox/GordonGroup/ben_kumwenda_genomes'
-kraken_inhandle = f'{root_dir}/kraken2/results/2020.09.30/2020.10.01.parsed_results.txt'
+kraken_inhandle = f'{root_dir}/kraken2/results/2020.09.30/2020.10.01.parsed_results.v2.txt'
 sistr_inhandle = f'{root_dir}/sistr/results/2020.09.25/2020.09.25.ben_k_sistr_results.tsv'
 assembly_stats_inhandle = f'{root_dir}/qc/results/2020.09.28/2020.09.28.assembly_stats.ben_k.tsv'
 
 if __name__ == '__main__':
-    main()
+    main(kraken_inhandle, sistr_inhandle, assembly_stats_inhandle)
